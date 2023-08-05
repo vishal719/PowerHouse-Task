c38fb32f93f585168f0d6a8e9ba997ec0e9dee51
VishalK <vkarmakar321@gmail.com>
2023-08-06 03:12:30 +0530

    added current day forecast

--- assignment/app/src/main/java/com/example/powerhouseassignment/MainActivity.kt
+++ assignment/app/src/main/java/com/example/powerhouseassignment/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.core.widget.doOnTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.example.powerhouseassignment.adapter.WeatherToday
 import com.example.powerhouseassignment.databinding.ActivityMainBinding
 import com.example.powerhouseassignment.ui.WeatherViewModel
 import java.net.URI
@@ -25,14 +27,17 @@ class MainActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityMainBinding
   lateinit var viM: WeatherViewModel
+  lateinit var adapter: WeatherToday
+  val url: String = "http://openweathermap.org/img/w/10d.png"
 
-  val url : String = "http://openweathermap.org/img/w/10d.png"
   @RequiresApi(Build.VERSION_CODES.O)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     viM = ViewModelProvider(this).get(WeatherViewModel::class.java)
-
+    adapter = WeatherToday()
+    val llayout: LinearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+    binding.recyclerView.layoutManager = llayout
 
     binding.searchCity.setOnEditorActionListener { _, actionId, event ->
       if (actionId == EditorInfo.IME_ACTION_DONE ||
@@ -41,8 +46,7 @@ class MainActivity : AppCompatActivity() {
         // Handle the enter key press here
         val enteredText = binding.searchCity.text.toString()
         viM.getWeather(enteredText)
-        Toast.makeText(baseContext,"$enteredText",Toast.LENGTH_SHORT).show()
-
+        Toast.makeText(baseContext, "$enteredText", Toast.LENGTH_SHORT).show()
         // Do something with enteredText
         true
       } else {
@@ -67,19 +71,117 @@ class MainActivity : AppCompatActivity() {
 
       binding.layoutWeather.weatherHumidity.text = it.main!!.humidity.toString()
       binding.layoutWeather.weatherWind.text = it.wind?.speed.toString()
-      binding.layoutWeather.weatherRain.text = it.clouds.toString()
-
+      binding.layoutWeather.weatherRain.text = it.clouds?.all.toString()
       val inputFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
       val date = inputFormat.parse(it.dtTxt!!)
       val outputFormat = SimpleDateFormat("d MMMM EEEE", Locale.getDefault())
       val dateanddayname = outputFormat.format(date!!)
       binding.layoutWeather.weatherDate.text = dateanddayname.toString()
-
-      Glide.with(binding.layoutWeather.weatherImage.context)
-        .load(url)
-        .into(binding.layoutWeather.weatherImage)
-
       // setting the icon
+      for (i in it.weather) {
+        if (i.icon == "01d") {
+          Glide.with(binding.layoutWeather.weatherImage.context)
+            .load(R.drawable.oned)
+            .into(binding.layoutWeather.weatherImage)
+
+        }
+
+        if (i.icon == "01n") {
+          Glide.with(binding.layoutWeather.weatherImage.context)
+            .load(R.drawable.onen)
+            .into(binding.layoutWeather.weatherImage)
+
+        }
+
+        if (i.icon == "02d") {
+          Glide.with(binding.layoutWeather.weatherImage.context)
+            .load(R.drawable.twod)
+            .into(binding.layoutWeather.weatherImage)
+
+        }
+
+
+        if (i.icon == "02n") {
+          Glide.with(binding.layoutWeather.weatherImage.context)
+            .load(R.drawable.twon)
+            .into(binding.layoutWeather.weatherImage)
+
+        }
+
+
+        if (i.icon == "03d" || i.icon == "03n") {
+          Glide.with(binding.layoutWeather.weatherImage.context)
+            .load(R.drawable.threedn)
+            .into(binding.layoutWeather.weatherImage)
+
+        }
+
+
+
+        if (i.icon == "10d") {
+          Glide.with(binding.layoutWeather.weatherImage.context)
+            .load(R.drawable.tend)
+            .into(binding.layoutWeather.weatherImage)
+
+        }
+
+
+        if (i.icon == "10n") {
+          Glide.with(binding.layoutWeather.weatherImage.context)
+            .load(R.drawable.tenn)
+            .into(binding.layoutWeather.weatherImage)
+
+        }
+
+
+        if (i.icon == "04d" || i.icon == "04n") {
+          Glide.with(binding.layoutWeather.weatherImage.context)
+            .load(R.drawable.fourdn)
+            .into(binding.layoutWeather.weatherImage)
+
+        }
+
+
+        if (i.icon == "09d" || i.icon == "09n") {
+          Glide.with(binding.layoutWeather.weatherImage.context)
+            .load(R.drawable.ninedn)
+            .into(binding.layoutWeather.weatherImage)
+
+        }
+
+
+
+        if (i.icon == "11d" || i.icon == "11n") {
+          Glide.with(binding.layoutWeather.weatherImage.context)
+            .load(R.drawable.elevend)
+            .into(binding.layoutWeather.weatherImage)
+
+        }
+
+
+        if (i.icon == "13d" || i.icon == "13n") {
+          Glide.with(binding.layoutWeather.weatherImage.context)
+            .load(R.drawable.thirteend)
+            .into(binding.layoutWeather.weatherImage)
+
+        }
+
+        if (i.icon == "50d" || i.icon == "50n") {
+          Glide.with(binding.layoutWeather.weatherImage.context)
+            .load(R.drawable.fiftydn)
+            .into(binding.layoutWeather.weatherImage)
+
+        }
+
+      }
+    })
+
+    viM.todayWeatherLiveData.observe(this, Observer {
+      val setNewlist = it as List<WeatherList>
+
+
+      adapter.setList(setNewlist)
+      binding.recyclerView.adapter = adapter
     })
 
   }
---
+++ assignment/app/src/main/java/com/example/powerhouseassignment/adapter/WeatherTodayAdapter.kt
@@ -0,0 +1,181 @@
+package com.example.powerhouseassignment.adapter
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.powerhouseassignment.R
+import com.example.powerhouseassignment.WeatherList
+import java.lang.Math.abs
+import java.text.SimpleDateFormat
+import java.time.*
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+class WeatherToday : RecyclerView.Adapter<TodayHolder>() {
+
+  private var listOfTodayWeather = listOf<WeatherList>()
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodayHolder {
+    val view =
+      LayoutInflater.from(parent.context).inflate(R.layout.item_weather_card, parent, false)
+    return TodayHolder(view)
+
+  }
+
+  override fun getItemCount(): Int {
+    return listOfTodayWeather.size
+
+  }
+
+  @RequiresApi(Build.VERSION_CODES.O)
+  @SuppressLint("SetTextI18n")
+  override fun onBindViewHolder(holder: TodayHolder, position: Int) {
+    val todayForeCast = listOfTodayWeather[position]
+
+
+
+
+    holder.timeDisplay.text = todayForeCast.dtTxt!!.substring(11, 16).toString()
+    val temperatureFahrenheit = todayForeCast.main?.temp
+    val temperatureCelsius = (temperatureFahrenheit?.minus(273.15))
+    val temperatureFormatted = String.format("%.2f", temperatureCelsius)
+
+
+    holder.tempDisplay.text = "$temperatureFormatted°"
+    holder.speedDisplay.text = "${todayForeCast.wind?.speed} km/h"
+
+    val calendar = Calendar.getInstance()
+// Define the desired format
+    val dateFormat = SimpleDateFormat("HH::mm")
+    val formattedTime = dateFormat.format(calendar.time)
+    val timeofapi = todayForeCast.dtTxt!!.split(" ")
+    val partafterspace = timeofapi[1]
+
+    Log.e("time", " formatted time:${formattedTime}, timeofapi: ${partafterspace}")
+
+
+    for (i in todayForeCast.weather) {
+      if (i.icon == "01d") {
+        Glide.with(holder.imageDisplay.context)
+          .load(R.drawable.oned)
+          .into(holder.imageDisplay)
+
+      }
+
+      if (i.icon == "01n") {
+        Glide.with(holder.imageDisplay.context)
+          .load(R.drawable.onen)
+          .into(holder.imageDisplay)
+
+      }
+
+      if (i.icon == "02d") {
+        Glide.with(holder.imageDisplay.context)
+          .load(R.drawable.twod)
+          .into(holder.imageDisplay)
+
+      }
+
+
+      if (i.icon == "02n") {
+        Glide.with(holder.imageDisplay.context)
+          .load(R.drawable.twon)
+          .into(holder.imageDisplay)
+
+      }
+
+
+      if (i.icon == "03d" || i.icon == "03n") {
+        Glide.with(holder.imageDisplay.context)
+          .load(R.drawable.threedn)
+          .into(holder.imageDisplay)
+
+      }
+
+
+
+      if (i.icon == "10d") {
+        Glide.with(holder.imageDisplay.context)
+          .load(R.drawable.tend)
+          .into(holder.imageDisplay)
+
+      }
+
+
+      if (i.icon == "10n") {
+        Glide.with(holder.imageDisplay.context)
+          .load(R.drawable.tenn)
+          .into(holder.imageDisplay)
+
+      }
+
+
+      if (i.icon == "04d" || i.icon == "04n") {
+        Glide.with(holder.imageDisplay.context)
+          .load(R.drawable.fourdn)
+          .into(holder.imageDisplay)
+
+      }
+
+
+      if (i.icon == "09d" || i.icon == "09n") {
+        Glide.with(holder.imageDisplay.context)
+          .load(R.drawable.ninedn)
+          .into(holder.imageDisplay)
+
+      }
+
+
+
+      if (i.icon == "11d" || i.icon == "11n") {
+        Glide.with(holder.imageDisplay.context)
+          .load(R.drawable.elevend)
+          .into(holder.imageDisplay)
+
+      }
+
+
+      if (i.icon == "13d" || i.icon == "13n") {
+        Glide.with(holder.imageDisplay.context)
+          .load(R.drawable.thirteend)
+          .into(holder.imageDisplay)
+
+      }
+
+      if (i.icon == "50d" || i.icon == "50n") {
+        Glide.with(holder.imageDisplay.context)
+          .load(R.drawable.fiftydn)
+          .into(holder.imageDisplay)
+
+      }
+
+    }
+
+  }
+
+  fun setList(listOfToday: List<WeatherList>) {
+    this.listOfTodayWeather = listOfToday
+
+  }
+
+}
+
+class TodayHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+  val imageDisplay: ImageView = itemView.findViewById(R.id.weather_icon)
+  val tempDisplay: TextView = itemView.findViewById(R.id.weather_temppp)
+  val timeDisplay: TextView = itemView.findViewById(R.id.weather_time)
+  val speedDisplay: TextView = itemView.findViewById(R.id.weather_speed)
+
+}
+
+
+
